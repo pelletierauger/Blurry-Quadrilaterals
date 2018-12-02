7497acd518310193222ff6870ef5c4f440d06a83
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2018-12-02 18:53:30 -0500

    Create a first working version

--- sketch.js
+++ sketch.js
@@ -2,11 +2,31 @@ let looping = true;
 let socket, cnvs, ctx, canvasDOM;
 let fileName = "./frames/sketch";
 let maxFrames = 20;
+let gl, shaderProgram;
 
 function setup() {
     socket = io.connect('http://localhost:8080');
-    cnvs = createCanvas(windowWidth, windowWidth / 16 * 9);
-    ctx = cnvs.drawingContext;
+    cnvs = createCanvas(windowWidth, windowWidth / 16 * 9, WEBGL);
+    // gl = cnvs.getContext('webgl');
+    gl = cnvs.drawingContext;
+
+    // Clear the canvas
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    // Enable the depth test
+    gl.enable(gl.DEPTH_TEST);
+
+    // Clear the color buffer bit
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.enable(gl.BLEND);
+    // gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+    gl.blendFunc(gl.SRC_ALPHA, gl.DST_ALPHA);
+
+    // Set the view port
+    gl.viewport(0, 0, canvas.width, canvas.height);
+
     canvasDOM = document.getElementById('defaultCanvas0');
     frameRate(30);
     background(0);
@@ -15,14 +35,92 @@ function setup() {
     if (!looping) {
         noLoop();
     }
+
 }
 
 function draw() {
-    for (let i = 0; i < 500; i++) {
-        let x = random(width);
-        let y = random(height);
-        ellipse(x, y, 5);
-    }
+
+    rectangles = 0;
+
+    let t = frameCount;
+    let osc = 0.1;
+    let vertices = [];
+    let colors = [];
+    let indices = [];
+    let rectangle = makeQuad({
+        c: [0.1, 0.2, 0.9, 1.0],
+        v: [
+            [-0.5 + (Math.sin(t * 0.05) * osc), 0.5 + (Math.cos(t * 0.05) * osc)],
+            [0.5 + (Math.sin(t * 0.015) * osc), 0.5 + (Math.cos(t * 0.015) * osc)],
+            [0.5 + (Math.sin(t * 0.045) * osc), -0.5 + (Math.cos(t * 0.045) * osc)],
+            [-0.5 + (Math.sin(t * 0.025) * osc), -0.5 + (Math.cos(t * 0.025) * osc)]
+        ]
+    });
+
+
+    // var vertices = [-0.75, 0.0, 0.0, -0.5, -0.5, 0.0,
+    //     0.75, 0.0, 0.0, 0.5, 0.5, 0.0
+    // ];
+    // var colors = [0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0];
+    // var indices = [3, 2, 1, 3, 1, 0];
+
+    vertices = rectangle.vertices;
+    colors = rectangle.colors;
+    indices = rectangle.indices;
+    console.log(indices);
+
+    // Create an empty buffer object and store vertex data
+    var vertex_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    // Create an empty buffer object and store Index data
+    var Index_Buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+    // Create an empty buffer object and store color data
+    var color_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+    setShaders();
+    /* ======== Associating shaders to buffer objects =======*/
+
+    // Bind vertex buffer object
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+
+    // Bind index buffer object
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
+
+    // Get the attribute location
+    var coord = gl.getAttribLocation(shaderProgram, "coordinates");
+
+    // point an attribute to the currently bound VBO
+    gl.vertexAttribPointer(coord, 3, gl.FLOAT, false, 0, 0);
+
+    // Enable the attribute
+    gl.enableVertexAttribArray(coord);
+
+    // bind the color buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+
+    // get the attribute location
+    var color = gl.getAttribLocation(shaderProgram, "color");
+
+    // point attribute to the volor buffer object
+    gl.vertexAttribPointer(color, 4, gl.FLOAT, false, 0, 0);
+
+    // enable the color attribute
+    gl.enableVertexAttribArray(color);
+
+    /*============Drawing the Quad====================*/
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    //Draw the triangle
+    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+
     if (exporting && frameCount < maxFrames) {
         frameExport();
     }
